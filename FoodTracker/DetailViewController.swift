f9be905baf55a8cbf832c41147b9534d60421adc
Avishaan <avishaan@codeHatcher.com>
2015-05-14 18:24:17 -0700

    added detailed view controller, hooked up the bar button item and added some autolayout for a couple of iphones

---
+++ FoodTracker/DetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailViewController.swift
+//  FoodTracker
+//
+//  Created by Brown Magic on 5/14/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+  @IBOutlet weak var textView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func eatItBarButtonItemPressed(sender: UIBarButtonItem) {
+  }
+
+}
