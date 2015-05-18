47ebe40eb678131c021994479b621aaf51c14610
Avishaan <avishaan@codeHatcher.com>
2015-05-17 18:10:40 -0700

    refresh tableView data when the scope button index is changed

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -162,6 +162,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
   }
   
+  func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+    self.tableView.reloadData()
+  }
+  
   func makeRequest (searchString:String) {
 //    let url = NSURL(string: "https://api.nutritionix.com/v1_1/search/\(searchString)?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=\(kAppId)&appKey=\(kAppKey)")
 //    let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
